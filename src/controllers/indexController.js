40df36e736e19d7d4075e25a6c2fab127827b7e9
Guinaudeau Christine <comsamarche@gmail.com>
2017-04-22 13:05:00 +0200

    Mise en place de la session utilisateur

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -1,12 +1,21 @@
 'use strict';
 
 app
-	.controller('IndexController', function(userProvider , itemProvider){
+	.controller('IndexController', function($rootScope, userProvider , itemProvider, Auth){
 		var Index = this;
 
-		Index.auth = function auth(Index){
-			userProvider.authUser(Index);
+		Index.session = Auth;
+		Index.session.$onAuthStateChanged(function(firebaseUser) {
+	      Index.firebaseUser = firebaseUser;
+	    });
+
+		Index.auth = function (user){
+			userProvider.authUser(user);
 		}
+
+		Index.logout = function (){
+			userProvider.logout();
+		}		
 
 		Index.items = itemProvider.getItems();
 
--- src/services/usersProvider.js
+++ src/services/usersProvider.js
@@ -1,6 +1,6 @@
 'use strict';
 
-app.service('userProvider', function($firebaseAuth, Auth){
+app.service('userProvider', function($rootScope , $firebaseAuth, Auth){
 
 	// Create a new user
 	this.createUser = function(email, password){
@@ -12,15 +12,38 @@ app.service('userProvider', function($firebaseAuth, Auth){
 		});
 	}
 
-	// Verif user
+	// Check authentification
 	this.authUser = function(user){
 
 		Auth.$signInWithEmailAndPassword(user.email, user.password)
 		.then(function(authData) {
 		  console.log("Logged in as:", authData.uid);
+		  return authData;
 		}).catch(function(error) {
 		  console.error("Authentication failed:", error);
+		  return user;
+		});			
+	}
+
+	// Return authentification
+	this.getAuth = function(user){
+		var authData = Auth.$getAuth();
+		if (authData) {
+		  console.log("Is Logged as:", authData.email);	
+		  return authData;
+		} else {
+		  console.log("Logged out");
+		}
+	}
+
+	// // Logout
+	this.logout = function(){
+		Auth.$signOut().then(function() {
+		  console.log('Signed Out');
+		}, function(error) {
+		  console.error('Sign Out Error', error);
 		});
 	}
+
 
 });
